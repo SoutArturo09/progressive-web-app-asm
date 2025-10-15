266f2cae3a3f6b40fd17b610fb2e0296cf509867
Arturo Sandoval <as5237189@gmail.com>
2025-10-14 20:13:14 -0600

    feat: sw with push notifications

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.m1908md50qo"
+    "revision": "0.ivip7kr6edg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/sw.js
+++ src/sw.js
@@ -129,8 +129,12 @@ async function syncTasks() {
 
 self.addEventListener('push', (event) => {
   const data = event.data?.json() ?? {};
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: '/icons/icon-192.png',
-  });
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icons/icon-192.png',
+    })
+  );
 });
+
+
